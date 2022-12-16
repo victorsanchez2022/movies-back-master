810c1cb3c4e11cece26ed65f551d6fc8e93fc544
victorsanchez2022 <victor.sanchez@o365.unab.edu.co>
2022-12-16 08:56:58 -0500

    update service

--- src/main/java/com/app/movie/repository/ScoreRepository.java
+++ src/main/java/com/app/movie/repository/ScoreRepository.java
@@ -11,6 +11,7 @@ import com.app.movie.interfaces.IScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -19,29 +20,35 @@ import java.util.Optional;
  */
 @Repository
 public class ScoreRepository {
-    
+
     @Autowired
     IScoreRepository repository;
-    
+
     public Iterable<Score> getAll(){
         return repository.findAll();
     }
-    
+
     public Optional<Score> findById(String id){
         Optional<Score> response= repository.findById(id);
         return response;
     }
-    
+
+    public List<Score> findByMovieAndClient(String movieId,String clientId){
+        List<Score> response= repository.getScoreByMovieAndClient(movieId,clientId);
+        return response;
+    }
+
+
     public Boolean existsById(String id){
         return repository.existsById(id);
     }
-    
+
     public void deleteById(String id){
         repository.deleteById(id);
     }
-    
+
     public Score save(Score score){
         return repository.save(score);
     }
-    
+
 }
